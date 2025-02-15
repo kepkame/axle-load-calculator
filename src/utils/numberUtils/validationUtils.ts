11d4d856d74c9c01715136672b835d1ea900c439
kepkame <kepkame@gmail.com>
2025-02-15 13:22:41 +0300

    ALC-8 The “processBlurValue” function has been removed. The function “handleNumberInputBlur” is used instead.

--- src/utils/numberUtils/validationUtils.ts
+++ src/utils/numberUtils/validationUtils.ts
@@ -1,30 +1,4 @@
 /**
- * Processing of value at blur event:
- * - If the value is empty or equal to '-', an empty string is returned.
- * - Converting a string to a number.
- * - Limiting a value between min and max.
- * - Formatting a number using formatter.
- */
-export const processBlurValue = (
-  value: string,
-  min: number,
-  max: number,
-  decimalPlaces: number,
-  formatter: (value: number, decimalPlaces: number) => string,
-): string => {
-  if (value === '-' || value === '') {
-    return '';
-  }
-  const numberValue = Number(value);
-  if (isNaN(numberValue)) {
-    return '';
-  }
-  const clampedValue = Math.min(Math.max(numberValue, min), max);
-
-  return formatter(clampedValue, decimalPlaces);
-};
-
-/**
  * Limits the number of decimal places in the range of 0 to 3.
  */
 export const normalizeDecimalPlaces = (decimalPlaces: number): number => {
