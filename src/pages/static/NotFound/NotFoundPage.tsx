8a8a6b59c68a3018a9c4878c6ad278b0e9c431f0
kepkame <kepkame@gmail.com>
2025-10-20 14:06:18 +0300

    refactor: redesign 404 page with image, columns layout and home button

--- src/pages/static/NotFound/NotFoundPage.tsx
+++ src/pages/static/NotFound/NotFoundPage.tsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+import notFoundWebp from '@assets/images/not-found/404.webp';
+import notFoundJpg from '@assets/images/not-found/404.jpg';
+import styles from './NotFoundPage.module.scss';
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>Страница не найдена</h1>
+    <div className={styles.wrapper}>
+      <div className={clsx(styles.col, styles['col--text'])}>
+        <h1 className={styles.title}>Страница не найдена</h1>
+        <Link to="/" className="btn">
+          На главную
+        </Link>
+      </div>
+
+      <div className={clsx(styles.col, styles['col--media'])}>
+        <picture>
+          <source srcSet={notFoundWebp} type="image/webp" />
+          <img src={notFoundJpg} alt="Иллюстрация: грузовик и дорожный знак с цифрами 404" />
+        </picture>
+      </div>
     </div>
   );
 };
